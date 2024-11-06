9b43464c88479604ed37f0d183add4963cd8e303
Damian-Rios <damianjrios5@gmail.com>
2024-11-06 15:36:26 -0600

    set up views and urls

--- expenseTracker/expenses/urls.py
+++ expenseTracker/expenses/urls.py
@@ -2,6 +2,11 @@ from django.urls import path
 
 from . import views
 
+app_name = 'expenses'
 urlpatterns = [
     path("", views.index, name="index"),
+    path("add/", views.add_expense, name="add_expense"),
+    path("edit/<int:id>", views.edit_expense, name="edit_expense"),
+    path("delete/<int:id>", views.delete_expense, name="delete_expense"),
+
 ]
--- expenseTracker/expenses/views.py
+++ expenseTracker/expenses/views.py
@@ -1,5 +1,19 @@
 from django.http import HttpResponse
+from django.shortcuts import render
+from django.template import loader
+from .models import Expense
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    expenses = Expense.objects.filter(user=request.user)
+    context = {'expenses': expenses}
+    return render(request, "expenses/index.html", context)
+
+def add_expense(request):
+    return HttpResponse("Hello, world. You're adding an expense.")
+
+def edit_expense(request):
+    return HttpResponse("Hello, world. You're editing an expense.")
+
+def delete_expense(request):
+    return HttpResponse("Hello, world. You're deleting an expense.")
