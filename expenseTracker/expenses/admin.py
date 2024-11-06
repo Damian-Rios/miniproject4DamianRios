35c486b95ad22910866d348305b0ddf74a403515
Damian-Rios <damianjrios5@gmail.com>
2024-11-06 13:27:52 -0600

    set up admin

--- expenseTracker/expenses/admin.py
+++ expenseTracker/expenses/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import Expense
 
-# Register your models here.
+
+class ExpenseAdmin(admin.ModelAdmin):
+    # Display key fields for quick review
+    list_display = ('user', 'amount', 'category', 'description', 'date')
+
+    # Allow filtering by user, category, and date
+    list_filter = ('user', 'category', 'date')
+
+    # Enable searching for specific expenses by category or description
+    search_fields = ('category', 'description')
+
+
+admin.site.register(Expense, ExpenseAdmin)
